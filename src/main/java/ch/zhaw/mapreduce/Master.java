49fcb814f5de9a118b3b35ed39a0693e996b101a
Reto Hablützel <rethab.ch@gmail.com>
2013-01-22 21:17:13 +0100

    german output in master

--- src/main/java/ch/zhaw/mapreduce/Master.java
+++ src/main/java/ch/zhaw/mapreduce/Master.java
@@ -16,11 +16,11 @@ import javax.inject.Inject;
 import ch.zhaw.mapreduce.registry.MapReduceTaskUUID;
 
 public final class Master {
-	
+
 	private final Pool pool;
 
 	private final String mapReduceTaskUUID;
-	
+
 	private final WorkerTaskFactory runnerFactory;
 
 	@Inject
@@ -31,28 +31,35 @@ public final class Master {
 	}
 
 	public Map<String, String> runComputation(final MapInstruction mapInstruction,
-			final CombinerInstruction combinerInstruction,
-			final ReduceInstruction reduceInstruction, Iterator<String> input) throws InterruptedException {
+			final CombinerInstruction combinerInstruction, final ReduceInstruction reduceInstruction,
+			Iterator<String> input) throws InterruptedException {
 
 		// MAP
 		// Alle derzeitigen aufgaben die ausgeführt werden
+		System.out.println("Starte MAP Phase");
 		Set<WorkerTask> mapTasks = runMap(mapInstruction, combinerInstruction, input);
 		Set<Worker> mapResults = waitForWorkers(mapTasks);
+		System.out.println("MAP Phase fertig");
 
 		// SHUFFLE
+		System.out.println("Starte SHUFFLE Phase");
 		Map<String, List<KeyValuePair>> shuffleResults = runShuffle(mapResults);
+		System.out.println("SHUFFLE Phase fertig");
 
 		// REDUCE
+		System.out.println("Starte REDUCE Phase");
 		Set<WorkerTask> reduceTasks = runReduce(reduceInstruction, shuffleResults);
 		Set<Worker> reduceResults = waitForWorkers(reduceTasks);
-		
+		System.out.println("REDUCE Phase fertig");
+
 		Map<String, String> results = collectResults(reduceResults);
 
 		this.pool.cleanResults(mapReduceTaskUUID);
 		return results;
 	}
-	
-	Set<WorkerTask> runMap(MapInstruction mapInstruction, CombinerInstruction combinerInstruction, Iterator<String> input) {
+
+	Set<WorkerTask> runMap(MapInstruction mapInstruction, CombinerInstruction combinerInstruction,
+			Iterator<String> input) {
 		Set<WorkerTask> activeWorkerTasks = new LinkedHashSet<WorkerTask>();
 		// reiht für jeden Input - Teil einen MapWorkerTask in den Pool ein
 		while (input.hasNext()) {
@@ -60,16 +67,16 @@ public final class Master {
 			String inputUUID = UUID.randomUUID().toString();
 			String todo = input.next();
 
-			MapWorkerTask mapTask = runnerFactory.createMapWorkerTask(mapReduceTaskUUID,
-					mapInstruction, combinerInstruction, inputUUID, todo);
+			MapWorkerTask mapTask = runnerFactory.createMapWorkerTask(mapReduceTaskUUID, mapInstruction,
+					combinerInstruction, inputUUID, todo);
 
 			activeWorkerTasks.add(mapTask);
 			mapTask.runMapTask();
 		}
 		return activeWorkerTasks;
 	}
-	
-	Map<String, List<KeyValuePair>> runShuffle (Collection<Worker> mapResults) {
+
+	Map<String, List<KeyValuePair>> runShuffle(Collection<Worker> mapResults) {
 		Map<String, List<KeyValuePair>> reduceTasks = new HashMap<String, List<KeyValuePair>>();
 		for (Worker curMapResult : mapResults) {
 			for (KeyValuePair curKeyValuePair : curMapResult.getMapResults(mapReduceTaskUUID)) {
@@ -84,7 +91,7 @@ public final class Master {
 		}
 		return reduceTasks;
 	}
-	
+
 	Set<WorkerTask> runReduce(ReduceInstruction reduceInstruction, Map<String, List<KeyValuePair>> shuffleResults) {
 		Set<WorkerTask> reduceTasks = new LinkedHashSet<WorkerTask>();
 		// reiht für jeden Input - Teil einen MapWorkerTask in den Pool ein
@@ -98,11 +105,11 @@ public final class Master {
 		}
 		return reduceTasks;
 	}
-	
-	Map<String, String> collectResults (Set<Worker> reduceResults) {
+
+	Map<String, String> collectResults(Set<Worker> reduceResults) {
 		Map<String, String> resultStructure = new HashMap<String, String>();
-		for(Worker curWorker : reduceResults) {
-			for(KeyValuePair storedValue : curWorker.getReduceResults(mapReduceTaskUUID)) {
+		for (Worker curWorker : reduceResults) {
+			for (KeyValuePair storedValue : curWorker.getReduceResults(mapReduceTaskUUID)) {
 				resultStructure.put(storedValue.getKey(), storedValue.getValue());
 			}
 		}
@@ -125,7 +132,12 @@ public final class Master {
 				case COMPLETED:
 					toRemove.add(curWorkerTask);
 					workerWithResults.add(curWorkerTask.getWorker());
+					break;
 				case FAILED:
+					 //System.out.println("State:  " + curWorkerTask.getCurrentState());
+					 //System.out.println("Worker: " + curWorkerTask.getWorker());
+					 break;
+				default:
 
 					// Falls es diesen Status überhaupt gibt
 
