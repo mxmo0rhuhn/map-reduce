dbb3e752cba3acb39fcb9a17042267a765e585d0
mo0rhuhn <max-schrimpf@hotmail.com>
2013-04-30 16:25:21 +0200

    generelles interface für MR applikationen

---
+++ src/main/java/ch/zhaw/mapreduce/MapReduce.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package ch.zhaw.mapreduce;
+
+/**
+ * Wär cool mit Builder Pattern
+ * @author Max
+ *
+ */
+public interface MapReduce {
+	
+	/**
+	 * Startet die eingebundene Implementation des MapReduce - sei es ein Server oder ein client. 
+	 * Für Server => ServerStarter.start()
+	 */
+	start 
+	stop 
+	
+	configure
+	newMRTask
+}
