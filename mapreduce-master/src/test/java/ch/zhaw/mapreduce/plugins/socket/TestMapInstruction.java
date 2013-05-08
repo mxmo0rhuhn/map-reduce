f13efbe5b7c291656c64ba7c97680a16fac92df9
rethab <rethab@rethab.ch>
2013-05-08 15:37:27 +0200

    moved logging to shared

--- mapreduce-master/src/test/java/ch/zhaw/mapreduce/plugins/socket/TestMapInstruction.java
+++ mapreduce-master/src/test/java/ch/zhaw/mapreduce/plugins/socket/TestMapInstruction.java
@@ -5,10 +5,16 @@ import ch.zhaw.mapreduce.MapInstruction;
 
 public class TestMapInstruction implements MapInstruction {
 
-	private static final long serialVersionUID = -5951237460480827687L;
-
 	@Override
 	public void map(MapEmitter emitter, String input) {
+		try {
+			System.out.println("sleeping");
+			Thread.sleep(500);
+			System.out.println("done sleeping");
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		emitter.emitIntermediateMapResult("key", input);
 	}
 
--- mapreduce-master/src/test/java/ch/zhaw/mapreduce/plugins/socket/TestSocketServer.java
+++ mapreduce-master/src/test/java/ch/zhaw/mapreduce/plugins/socket/TestSocketServer.java
@@ -14,26 +14,29 @@ import de.root1.simon.Registry;
 import de.root1.simon.Simon;
 
 public class TestSocketServer {
-	
+
 	private static final Logger LOG = Logger.getLogger(TestSocketServer.class.getName());
 
 	public static void main(String[] args) throws Exception {
 		Injector injector = Guice.createInjector(new MapReduceConfig()).createChildInjector(new SocketServerConfig());
-//		ServerPluginPartNameMeBetter server = injector.getInstance(ServerPluginPartNameMeBetter.class);
-//		server.bind();
-//
+		// ServerPluginPartNameMeBetter server = injector.getInstance(ServerPluginPartNameMeBetter.class);
+		// server.bind();
+		//
 		Pool p = injector.getInstance(Pool.class);
 		SocketWorkerFactory swFactory = injector.getInstance(SocketWorkerFactory.class);
-//		
-//		for (int i = 0; i < 10; i++) {
-//			MapWorkerTask mapTask = new MapWorkerTask("myMapReduceId", "myTaskId"+i, new TestMapInstruction(), null, "value"+i);
-//			p.enqueueWork(mapTask);
-//		}
-		
+		//
+		// for (int i = 0; i < 10; i++) {
+		// MapWorkerTask mapTask = new MapWorkerTask("myMapReduceId", "myTaskId"+i, new TestMapInstruction(), null,
+		// "value"+i);
+		// p.enqueueWork(mapTask);
+		// }
+
 		LOG.info("I, " + MapReduceUtil.getLocalIp() + ", am thee Master and thou shalt be my Slaves!");
 		Registry reg = Simon.createRegistry(4753);
 		reg.bind("MapReduceSocketMaster", new RegistrationServerImpl(p, swFactory));
-		p.enqueueWork(new MapWorkerTask("mrtUuid", "tUuid", new TestMapInstruction(), null, "input"));
+		for (int i = 0; i < 1000; i++) {
+			p.enqueueWork(new MapWorkerTask("mrtUuid", "tUuid" + i, new TestMapInstruction(), null, "input"));
+		}
 	}
 
 }
