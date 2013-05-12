90aed8fb7053dfb22655777e206294d3487a8cc6
rethab <rethab@rethab.ch>
2013-05-12 22:57:09 +0200

    only log map function

--- mapreduce-client-socket/start.sh
+++ mapreduce-client-socket/start.sh
@@ -6,4 +6,4 @@
 #     Argumente für den Master-Server, Master-Port und Anzahl Worker, die dieser Client
 #     starten soll, übergeben.
 
-java -Djava.util.logging.config.file=/home/rethab/ptemp/map-reduce/mapreduce-shared/src/main/resources/logging.properties -jar target/mapreduce-client-socket-0.9-SNAPSHOT-jar-with-dependencies.jar localhost 4753 1
+java -Djava.util.logging.config.file=logging.properties -jar target/mapreduce-client-socket-0.9-SNAPSHOT-jar-with-dependencies.jar localhost 4753 1
