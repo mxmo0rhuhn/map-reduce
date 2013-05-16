bbabe01530fc8ca8dfebd2b704c878b92d708d1a
mo0rhuhn <max-schrimpf@hotmail.com>
2013-05-16 20:04:39 +0200

    enhanced Config

--- mapreduce-master/src/main/java/ch/zhaw/mapreduce/MapReduceConfig.java
+++ mapreduce-master/src/main/java/ch/zhaw/mapreduce/MapReduceConfig.java
@@ -1,5 +1,8 @@
 package ch.zhaw.mapreduce;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.UUID;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
@@ -19,8 +22,8 @@ import com.google.inject.matcher.Matchers;
 import com.google.inject.name.Names;
 
 /**
- * In dieser Klasse befinden sich die Bindings für Guice - also welche Implementationen für welche Interfaces verwendet
- * werden sollen.
+ * In dieser Klasse befinden sich die Bindings für Guice - also welche Implementationen für welche
+ * Interfaces verwendet werden sollen.
  * 
  * @author Reto
  * 
@@ -33,26 +36,57 @@ public class MapReduceConfig extends AbstractModule {
 	@Override
 	protected void configure() {
 
-		// AssistedInject Magic: Mit diesem FactoryModuleBuilder wird ein Binding für die RunnerFactory erstellt ohne,
+		// 10000L Milisekunden => 10 Sekunden
+		
+		// Intervall, in dem Statistiken geladen werden
+		long StatisticsPrinterTimeout = 10000L;
+
+		// Prozentzahl an RAMusage, ab der keine Tasks mehr angenommen werden
+		long minRemainingMemory = 10L;
+		// Zeit, die die Applikation keine neuen Tasks mehr annimt, wenn der RAM "voll" ist
+		long memoryFullSleepTime = 10000L;
+
+		// Plugins, die durch den Loader geladen werden sollen
+		String plugins = "Thread";
+
+		Properties prop = new Properties();
+		try {
+    		prop.load(new FileInputStream("mapReduce.properties"));
+			
+			StatisticsPrinterTimeout = Long.parseLong(prop.getProperty("StatisticsPrinterTimeout"));
+			memoryFullSleepTime = Long.parseLong(prop.getProperty("memoryFullSleepTime"));
+			minRemainingMemory = Long.parseLong(prop.getProperty("minRemainingMemory"));
+			plugins = prop.getProperty("plugins");
+			
+		} catch (IOException e) {
+			// konnten nicht geladen werden - weiter mit oben definierten defaults
+			e.printStackTrace();
+		}
+
+		// AssistedInject Magic: Mit diesem FactoryModuleBuilder wird ein Binding für die
+		// RunnerFactory erstellt ohne,
 		// dass wir eine tatsächliche Implementation bereitstellen.
 		install(new FactoryModuleBuilder().build(WorkerTaskFactory.class));
 		install(new FactoryModuleBuilder().build(MasterFactory.class));
-
 		bind(Pool.class);
 		bind(Loader.class);
 		bind(Shuffler.class).to(InMemoryShuffler.class);
 		bind(Persistence.class).to(FilePersistence.class);
-		bind(String.class).annotatedWith(Names.named("filepersistence.directory")).toInstance(System.getProperty("java.io.tmpdir") + "/socket/filepers/");
+
+		// für die FilePersistence
+		bind(String.class).annotatedWith(Names.named("filepersistence.directory")).toInstance(
+				System.getProperty("java.io.tmpdir") + "/socket/filepers/");
 		bind(String.class).annotatedWith(Names.named("filepersistence.ending")).toInstance(".ser");
-				
-		bind(String.class).annotatedWith(Names.named("plugins.property")).toInstance("mrplugins");
-		bind(Long.class).annotatedWith(Names.named("StatisticsPrinterTimeout")).toInstance(10000L);
-		// 10s
-		bind(Long.class).annotatedWith(Names.named("memoryFullSleepTime")).toInstance(10000L);
-		
-		// Prozentzahl an RAMusage, ab der keine Tasks mehr angenommen werden
-		bind(Long.class).annotatedWith(Names.named("minRemainingMemory")).toInstance(10L);
-		
+
+		// Zeug das aus dem properties file entnommen wird
+		bind(String.class).annotatedWith(Names.named("plugins")).toInstance(plugins);
+		bind(Long.class).annotatedWith(Names.named("StatisticsPrinterTimeout")).toInstance(
+				StatisticsPrinterTimeout);
+		bind(Long.class).annotatedWith(Names.named("memoryFullSleepTime")).toInstance(
+				memoryFullSleepTime);
+		bind(Long.class).annotatedWith(Names.named("minRemainingMemory")).toInstance(
+				minRemainingMemory);
+
 		// see PostConstructFeature
 		bindListener(Matchers.any(), new PostConstructFeature());
 	}
@@ -62,23 +96,23 @@ public class MapReduceConfig extends AbstractModule {
 	public Executor createPoolExec() {
 		return Executors.newSingleThreadExecutor(new NamedThreadFactory("PoolExecutor"));
 	}
-	
+
 	@Provides
 	@Named("SupervisorScheduler")
 	public ScheduledExecutorService poolSupervisor() {
 		return Executors.newScheduledThreadPool(1, new NamedThreadFactory("PoolSupervisor"));
 	}
-	
+
 	@Provides
 	@Named("mapReduceTaskUuid")
 	public String getMapReduceTaskUuid() {
 		return UUID.randomUUID().toString();
 	}
-	
+
 	@Provides
 	@Named("taskUuid")
 	public String getWorkerTaskUuid() {
 		return UUID.randomUUID().toString();
 	}
-	
+
 }
